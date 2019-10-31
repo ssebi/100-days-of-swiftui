c42838076d7182c1637a32b787898c1785a98a1c
Sebastian Vidrea <vidreasebastian@gmail.com>
2019-10-31 19:07:08 +0200

    Completed Challenge 1, day 22

---
+++ Day 22 - Project 2, part three/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,84 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Sebastian Vidrea on 13/10/2019.
+//  Copyright © 2019 App Company. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var message = ""
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var score = 0
+
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                }
+
+                ForEach(0 ..< 3) { number in
+                    Button(action: {
+                        self.flagTapped(number)
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                            .shadow(color: .black, radius: 2)
+                    }
+                }
+
+                Spacer()
+            }
+        }
+        .alert(isPresented: $showingScore) {
+            Alert(title: Text(scoreTitle), message: Text(message), dismissButton: .default(Text("Continue")) {
+                self.askQuestion()
+                })
+        }
+    }
+
+    private func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            score += 1
+            scoreTitle = "Correct"
+            message = "Your score is \(score)"
+        } else {
+            if score >= 1 {
+                score -= 1
+            }
+            scoreTitle = "Wrong"
+            message = "Your score is \(score)"
+        }
+
+        showingScore = true
+    }
+
+    private func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day 22 - Project 2, part three/README.md
@@ -0,0 +1,16 @@
+# Day 22 – Project 2: Guess the Flag, Part Three
+🔗 [HWS Day 22](https://www.hackingwithswift.com/100/swiftui/22)
+
+# 📝 Notes
+
+This is the final day of the project. So far, we've learnt about  how useful `VStacks`, `HStacks` and `VStacks` are and how we can use them to create complex layouts. We've also seen that `Colors` are views too, how to create `Gradients`, how to add images to `Buttons` and how to show `Alerts`.
+
+# Challenges
+## Challenge 1
+> Add an @State property to store the user’s score, modify it when they get an answer right or wrong, then display it in the alert.
+
+## Challenge 2
+> Show the player’s current score in a label directly below the flags.
+
+### Challenge 3
+> When someone chooses the wrong flag, tell them their mistake in your alert message – something like “Wrong! That’s the flag of France,” for example.
