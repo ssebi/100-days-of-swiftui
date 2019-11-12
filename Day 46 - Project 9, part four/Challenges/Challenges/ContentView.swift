d7ceb473a28e35895a8f7ea5a92df1e84c1a8552
Sebastian Vidrea <vidreasebastian@gmail.com>
2019-11-12 21:26:20 +0200

    Completed Challenge 2, Day 46.

--- Day 46 - Project 9, part four/Challenges/Challenges/ContentView.swift
+++ Day 46 - Project 9, part four/Challenges/Challenges/ContentView.swift
@@ -9,16 +9,21 @@
 import SwiftUI
 
 struct Triangle: Shape {
+    var arrowWidth: CGFloat
+    var animatableData: CGFloat {
+        get { arrowWidth }
+        set { self.arrowWidth = newValue }
+    }
+
     func path(in rect: CGRect) -> Path {
         var path = Path()
 
-        let offset: CGFloat = rect.width / 6
         path.move(to: CGPoint(x: rect.midX, y: rect.minY))
         path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY / 2))
-        path.addLine(to: CGPoint(x: rect.maxX / 2 - offset, y: rect.maxY / 2))
-        path.addLine(to: CGPoint(x: rect.maxX / 2 - offset, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX / 2 + offset, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX / 2 + offset, y: rect.maxY / 2))
+        path.addLine(to: CGPoint(x: rect.maxX / 2 - arrowWidth, y: rect.maxY / 2))
+        path.addLine(to: CGPoint(x: rect.maxX / 2 - arrowWidth, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX / 2 + arrowWidth, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX / 2 + arrowWidth, y: rect.maxY / 2))
         path.addLine(to: CGPoint(x: rect.maxX , y: rect.maxY / 2))
         path.closeSubpath()
 
@@ -27,11 +32,33 @@ struct Triangle: Shape {
 }
 
 struct ContentView: View {
+    @State var lineWidth: CGFloat = 1
+    let minValue: CGFloat = 0
+    let maxValue: CGFloat = 20
+
+    @State var arrowWidth: CGFloat = 10
+
     var body: some View {
         VStack {
-            Triangle()
-                .stroke(Color.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+            Triangle(arrowWidth: arrowWidth)
+                .stroke(Color.red, style: StrokeStyle(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
                 .frame(width: 300, height: 400)
+                .animation(.default)
+
+            Spacer()
+                .frame(height: 50)
+
+            VStack {
+                Text("Line Width")
+                Slider(value: $lineWidth, in: minValue...maxValue)
+                .padding()
+            }
+
+            VStack {
+                Text("Arrow Width")
+                Stepper("Arrow Width", value: $arrowWidth, in: 20...100, step: 30)
+                    .padding()
+            }
         }
     }
 }
