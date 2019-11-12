33a12f2408f0c6871f0dd5f43755be7a2ab75960
Sebastian Vidrea <vidreasebastian@gmail.com>
2019-11-12 22:41:39 +0200

    Completed Challenge 3, Day 46.

---
+++ Day 46 - Project 9, part four/ColorCyclingRectangle/ColorCyclingRectangle/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  ColorCyclingRectangle
+//
+//  Created by Sebastian Vidrea on 12/11/2019.
+//  Copyright © 2019 AppCompany. All rights reserved.
+//
+
+import SwiftUI
+
+struct ColorCyclingRectangle: View {
+    var amount = 0.0
+    var steps = 150
+
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Rectangle()
+                    .inset(by: CGFloat(value))
+                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
+                        self.color(for: value, brightness: 1),
+                        self.color(for: value, brightness: 0.5)
+                    ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+            }
+        }
+        .drawingGroup()
+    }
+
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
+
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
+
+struct ContentView: View {
+    @State private var colorCycle = 0.0
+
+    var body: some View {
+        VStack {
+            ColorCyclingRectangle(amount: colorCycle)
+                .frame(width: 300, height: 400)
+
+            Slider(value: $colorCycle)
+            .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
