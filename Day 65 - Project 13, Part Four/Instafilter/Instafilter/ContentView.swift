e9a5e1b2d77952f8568389aa55004b01ebca99cd
Sebastian Vidrea <vidreasebastian@gmail.com>
2019-12-08 21:30:52 +0200

    Completed Day 66

--- Day 65 - Project 13, Part Four/Instafilter/Instafilter/ContentView.swift
+++ Day 65 - Project 13, Part Four/Instafilter/Instafilter/ContentView.swift
@@ -13,10 +13,12 @@ struct ContentView: View {
     @State private var image: Image?
     @State private var filterIntensity = 0.5
 
+    @State private var showingFilterSheet = false
     @State private var showingImagePicker = false
     @State private var inputImage: UIImage?
+    @State private var processedImage: UIImage?
 
-    @State private var currentFilter = CIFilter.sepiaTone()
+    @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
     private let context = CIContext()
 
     var body: some View {
@@ -58,13 +60,22 @@ struct ContentView: View {
 
                 HStack {
                     Button("Change Filter") {
-
+                        self.showingFilterSheet = true
                     }
 
                     Spacer()
 
                     Button("Save") {
+                        guard let processedImage = self.processedImage else { return }
 
+                        let imageSaver = ImageSaver()
+                        imageSaver.successHandler = {
+                            print("Success")
+                        }
+                        imageSaver.errorHandler = {
+                            print("Oops: \($0.localizedDescription)")
+                        }
+                        imageSaver.writeToPhotoAlbum(image: processedImage)
                     }
                 }
             }
@@ -72,6 +83,25 @@ struct ContentView: View {
             .navigationBarTitle("Instafilter")
             .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
                 ImagePicker(image: self.$inputImage)
+            }
+            .actionSheet(isPresented: $showingFilterSheet) {
+                ActionSheet(title: Text("Select a filter"), buttons: [
+                    .default(Text("Crystalize")) {
+                        self.setFilter(CIFilter.crystallize()) },
+                    .default(Text("Edges")) {
+                        self.setFilter(CIFilter.edges()) },
+                    .default(Text("Gaussian Blur")) {
+                        self.setFilter(CIFilter.gaussianBlur()) },
+                    .default(Text("Pixellate")) {
+                        self.setFilter(CIFilter.pixellate()) },
+                    .default(Text("Sepia Tone")) {
+                        self.setFilter(CIFilter.sepiaTone()) },
+                    .default(Text("Unsharp Mask")) {
+                        self.setFilter(CIFilter.unsharpMask()) },
+                    .default(Text("Vignette")) {
+                        self.setFilter(CIFilter.vignette()) },
+                    .cancel()
+                ])
             }
         }
     }
@@ -85,13 +115,23 @@ struct ContentView: View {
     }
 
     func applyProcessing() {
-        currentFilter.intensity = Float(filterIntensity)
+        let inputKeys = currentFilter.inputKeys
+        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey) }
+
         guard let outputImage = currentFilter.outputImage else { return }
 
         if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
             let uiImage = UIImage(cgImage: cgimg)
             image = Image(uiImage: uiImage)
+            processedImage = uiImage
         }
+    }
+
+    func setFilter(_ filter: CIFilter) {
+        currentFilter = filter
+        loadImage()
     }
 }
 
---
+++ Day 66 - Project 13, Part Five/README.md
@@ -0,0 +1,18 @@
+# Day 66 - Project 13, Part Five
+
+🔗 [HWS Day 66](https://www.hackingwithswift.com/100/swiftui/66)
+
+# 📝 Notes
+
+Covered topics:
+
+- Customizing our filter using ActionSheet
+- Saving the filtered image using UIImageWriteToSavedPhotosAlbum()
+
+# Customizing our filter using ActionSheet
+
+- Now we've made the app more useful by adding the ability to apply different filters to our image.
+
+# Saving the filtered image using UIImageWriteToSavedPhotosAlbum()
+
+- We've now added the final touch to the app, by saving the image back in the photo library when the user taps the *Save* button.
