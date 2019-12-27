2dd5025807eae54d89176b908c82e8698c6b6646
Sebastian Vidrea <vidreasebastian@gmail.com>
2019-12-27 18:34:08 +0200

    Completed Day 88

---
+++ Day 88 - Project 17, Part Three/Flashzilla/Flashzilla/Views/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  Flashzilla
+//
+//  Created by Sebastian Vidrea on 27/12/2019.
+//  Copyright © 2019 AppCompany. All rights reserved.
+//
+
+import SwiftUI
+
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = CGFloat(total - position)
+        return self.offset(CGSize(width: 0, height: offset * 10))
+    }
+}
+
+struct ContentView: View {
+    @State private var cards = [Card](repeating: Card.example, count: 10)
+
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: self.cards[index]) {
+                            withAnimation {
+                                self.removeCard(at: index)
+                            }
+                        }
+                        .stacked(at: index, in: self.cards.count)
+                    }
+                }
+            }
+        }
+    }
+
+    func removeCard(at index: Int) {
+        cards.remove(at: index)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
